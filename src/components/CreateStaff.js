329b66f8a65bee8a0abd77a2932eef0cc2a9b3b8
lusigi <brianlusigi179@gmail.com>
2021-03-24 06:36:49 +0300

    third commit

--- src/components/CreateStaff.js
+++ src/components/CreateStaff.js
@@ -1,64 +1,153 @@
-import React from 'react'
-import Avatar from '@material-ui/core/Avatar';
-import { makeStyles } from '@material-ui/core/styles';
-import { IoArrowForwardCircleOutline } from "react-icons/io5"
-import TextField from '@material-ui/core/TextField';
-
+import React, { useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import AvatarGroup from "@material-ui/lab/AvatarGroup";
+import axios from "axios";
+import { makeStyles } from "@material-ui/core/styles";
+import { IoArrowForwardCircleOutline } from "react-icons/io5";
+import { Link } from "react-router-dom";
+import { Button } from "@material-ui/core";
+import img1 from "../images/img1.jpg";
+import img2 from "../images/img2.jpg";
+import img3 from "../images/img3.jpg";
+import img4 from "../images/img4.jpg";
 
 const useStyles = makeStyles((theme) => ({
-   
-    small: {
-      width: theme.spacing(3),
-      height: theme.spacing(3),
-    },
-    large: {
-      width: theme.spacing(12),
-      height: theme.spacing(12),
-
-
-    },
-  }));
+  small: {
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+  },
+  large: {
+    width: theme.spacing(9),
+    height: theme.spacing(9),
+  },
+}));
 
 function CreateStaff() {
-    const classes = useStyles();
-   return (
-        <div className="createStaff">
-            <div className="image-hero">
-                <Avatar className={classes.large} />
-                <div className="hero-text"> 
-                 <h2>Let's Add a new user</h2>
-                <p>Fill in all fields correctly to register a new Staff</p>
-                <IoArrowForwardCircleOutline className="icon-next"/>
-                </div>
-              
-            </div>
+  const [input, setInput] = useState({
+    surname: "",
+    idNumber: "",
+    otherNames: "",
+    employmentNumber: "",
+    region: "",
+    department: "",
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-            <div className="form-field">
-                    <form className={classes.root} noValidate autoComplete="off">
-                        <div>
-                        <TextField id="standard-basic" label="ID Number" />
-                        </div>
-                        <div>
-                        <TextField id="standard-basic" label="Employment Number" />
-                        </div>
-                        <div>
-                        <TextField id="standard-basic" label="Surname" />
-                        </div>
-                        <div>
-                        <TextField id="standard-basic" label="Other Names" />
-                        </div>
-                        <div>
-                        <TextField id="standard-basic" label="Region" />
-                        </div>
-                        <div>
-                        <TextField id="standard-basic" label="Department" />
-                        </div>
-                       
-                    
-                    </form>
-            </div>
+    setInput((prevInput) => {
+      return {
+        ...prevInput,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    const newStaff = {
+      surname: input.surname,
+      idNumber: input.idNumber,
+      otherNames: input.otherNames,
+      employmentNumber: input.employmentNumber,
+      region: input.region,
+      department: input.department,
+    };
+    axios.post("http://localhost:3001/api/users/addstaff", newStaff);
+  };
+
+  const classes = useStyles();
+  return (
+    <div className="createStaff">
+      <div className="image-hero">
+        <AvatarGroup max={5}>
+          <Avatar alt="Milda" src={img1} className={classes.large} />
+          <Avatar alt="Innocent" src={img2} className={classes.large} />
+          <Avatar alt="Hafsa" src={img3} className={classes.large} />
+          <Avatar alt="Wamae" src={img4} className={classes.large} />
+        </AvatarGroup>
+        <div className="hero-text">
+          <h2>Let's Add a new Staff Member</h2>
+          <p>Fill in all fields correctly to register a new Staff</p>
+          <IoArrowForwardCircleOutline className="icon-next" />
         </div>
-    )
+      </div>
+
+      <div className="form-field">
+        <form>
+          <div className="group-form">
+            <div className="flex-form left">
+              <label htmlFor="">Surname: </label>
+              <input
+                onChange={handleChange}
+                value={input.surname}
+                name="surname"
+                required
+              />
+            </div>
+            <div className="flex-form">
+              <label htmlFor="">ID Number</label>
+              <input
+                onChange={handleChange}
+                value={input.idNumber}
+                name="idNumber"
+                type="number"
+                required
+              />
+            </div>
+          </div>
+
+          <div className="flex-form large">
+            <label htmlFor="">Other Names </label>
+            <input
+              onChange={handleChange}
+              value={input.otherNames}
+              name="otherNames"
+              required
+            />
+          </div>
+          <div className="group-form">
+            <div className="flex-form left">
+              <label htmlFor="">Employment Number </label>
+              <input
+                onChange={handleChange}
+                value={input.employmentNumber}
+                name="employmentNumber"
+                type="number"
+                required
+              />
+            </div>
+            <div className="flex-form">
+              <label htmlFor="">Region </label>
+              <input
+                onChange={handleChange}
+                value={input.region}
+                name="region"
+                required
+              />
+            </div>
+          </div>
+
+          <div className="flex-form large">
+            <label htmlFor="">Department </label>
+            <input
+              onChange={handleChange}
+              value={input.department}
+              name="department"
+            />
+          </div>
+
+          <div className="submit-back">
+            <Link className="nav-home left" to="/">
+              Back
+            </Link>
+            <Button onClick={handleClick} className="submit">
+              Submit
+            </Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default CreateStaff
+export default CreateStaff;
--- src/components/DataGrid/DataGrid.js
+++ src/components/DataGrid/DataGrid.js
@@ -1,35 +1,32 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import { DataGrid } from "@material-ui/data-grid";
+import axios from "axios";
 
 const columns = [
   { field: "id", headerName: "ID", width: 70 },
-  { field: "identificationNumber", headerName: "ID number", width: 130 },
+  { field: "idNumber", headerName: "ID number", width: 130 },
   { field: "employmentNumber", headerName: "Employment number", width: 220 },
-  { field: "SurName", headerName: "Surname", width: 200 },
-  { field: "otherNames", headerName: "Other names", width: 200 },
-  { field: "Region", headerName: "Region", width: 130 },
-  { field: "Department", headerName: "Department", width: 200 },
-  { field: "employmentStatus", headerName: "Employment Status", width: 200 },
+  { field: "surname", headerName: "Surname", width: 200 },
+  { field: "otherNames", headerName: "Other names", width: 250 },
+  { field: "region", headerName: "Region", width: 250 },
+  { field: "department", headerName: "Department", width: 150 },
 ];
 
+export default function DataTable() {
+  const [rows, setRows] = useState([]);
 
+  useEffect(() => {
+    async function fetchData() {
+      const req = await axios.get("http://localhost:3001/api/users/allstaff");
 
+      setRows(req.data);
+    }
 
-
-const rows =[
-
-    {id:1, identificationNumber:31426501,employmentNumber:'9784',SurName :'Lusigi', otherNames:'Brian Isadia',Region: 'Nairobi', Department:'ICT', employmentStatus:'Employed' },
-    {id:2, identificationNumber:33326501,employmentNumber:'1223',SurName :'Simon', otherNames:'John Ombati',Region: 'Nairobi', Department:'ICT', employmentStatus:'Employed' },
-    {id:3, identificationNumber:31226501,employmentNumber:'1243',SurName :'Kendy', otherNames:'Sirengo',Region: 'Nairobi', Department:'ICT', employmentStatus:'Employed' },
-
-
-
-]
-
-export default function DataTable(){
-    return (
-        <div style = {{height:700,width:'100%'}}>
-         <DataGrid rows= {rows} columns={columns} pageSize={10} />
-     </div>
-    )
+    fetchData();
+  }, []);
+  return (
+    <div style={{ height: 700, width: "100%" }}>
+      <DataGrid rows={rows} columns={columns} pageSize={10} />
+    </div>
+  );
 }
